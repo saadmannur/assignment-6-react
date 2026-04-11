5d65879dc0c3c85a52363cb2275dac674448a711
saadmannur <saadmannur99@gmail.com>
2026-04-11 22:14:17 +0600

    finally it's ready to deploy

--- src/Component/ExploreSection/ExploreSection.jsx
+++ src/Component/ExploreSection/ExploreSection.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ExploreSection = () => {
     return (
-        <div className='container mx-auto  bg-linear-to-r from-blue-500 to-purple-700 text-white text-center p-15 space-y-5'>
+        <div className=' mx-auto  bg-linear-to-r from-blue-500 to-purple-700 text-white text-center p-15 space-y-5'>
             <div>
                 <h2 className='text-3xl font-bold md:text-5xl md:font-bold mb-5'>Ready to Transform Your Workflow?</h2>
                 <p>Join thousands of professionals who are already using Digitools to work smarter. Start your free trial today.</p>
--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import { FaXTwitter } from 'react-icons/fa6';
 
 const Footer = () => {
     return (
-        <div className='bg-[#101727] text-white px-20 md:px-48 pt-10 md:pt-30'>
+        <div className='bg-[#101727] text-white px-20 md:px-48 pt-10 md:pt-30 mx-auto'>
             <div className='grid grid-cols-2 md:grid-cols-6 gap-12 items-stretch pb-20 border-b border-gray-400'>
                 <div className='col-span-2'>
                     <h2 className='text-4xl font-bold mb-5'>DigiTools</h2>
